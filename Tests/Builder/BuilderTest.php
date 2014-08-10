2109618b8620b4a426c5beece988d8bc236d5dd6
prophet777 <johann_27@hotmail.fr>
2014-08-10 03:07:05 +0200

    Add question state event on computation (valid / invalid)

---
+++ Tests/Builder/BuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* This file is part of the PhpStorm.
+* (c) johann (johann_27@hotmail.fr)
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+**/
+
+namespace Certificationy\Component\Certy\Tests\Builder;
+
+
+class BuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBuild()
+    {
+
+    }
+}
