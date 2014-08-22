4b334662e3691077e2d30d0df1a008efbc023032
prophet777 <johann_27@hotmail.fr>
2014-08-22 20:21:35 +0200

    Write builder test

--- Tests/Builder/BuilderTest.php
+++ Tests/Builder/BuilderTest.php
@@ -9,9 +9,60 @@
 
 namespace Certificationy\Component\Certy\Tests\Builder;
 
+use Certificationy\Component\Certy\Builder\Builder;
+use Certificationy\Component\Certy\Context\CertificationContext;
+use Certificationy\Component\Certy\Model\Certification;
+
 class BuilderTest extends \PHPUnit_Framework_TestCase
 {
+    public function testCreateCertification()
+    {
+        $this->assertInstanceOf(Certification::CLASS, Builder::createCertification());
+    }
+
     public function testBuild()
+    {
+        $collector = $this->getMock('Certificationy\Component\Certy\Collector\CollectorInterface');
+
+        $context = $this->getMockBuilder(CertificationContext::CLASS)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $builder = new Builder($collector);
+
+        $certification = $builder->build($context);
+
+        $this->assertInstanceOf(Certification::CLASS, $certification);
+    }
+
+    public function testAddBuilderPass()
+    {
+        $builderPass = $this->getMock('Certificationy\Component\Certy\Builder\BuilderPassInterface');
+
+        $builder = new Builder();
+        $builder->addBuilderPass($builderPass);
+
+        $this->assertEquals(
+            [$builderPass],
+            \PHPUnit_Framework_Assert::readAttribute($builder, 'builderPass')
+        );
+    }
+
+    public function testCollector()
+    {
+        $builder = new Builder();
+
+        $this->assertInstanceOf(
+            'Certificationy\Component\Certy\Collector\CollectorInterface',
+            $builder->getCollector()
+        );
+    }
+
+    /**
+     *  Test this function now is a waste of time and pain, let's explode it before.
+     */
+    public function testNormalize()
     {
 
     }
